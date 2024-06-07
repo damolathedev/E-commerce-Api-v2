b9e664420c4d9793504249f663458146eccf2e9c
damolathedev <damolathedev@gmail.com>
2024-06-07 11:06:01 +0100

    aded docs

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
 
+const rateLimiter = require('express-rate-limit')
+const helment = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
+
 const connectDB = require('./db/connect')
 
 const authRouter = require('./routes/authRoutes')
@@ -19,6 +25,18 @@ const orderRoutes = require('./routes/orderRoutes')
 const notFoundMiddleWare = require('./middleware/not-found')
 const errorHandlerMiddleWare = require('./middleware/error-handler')
 
+app.set('trust proxy', 1)
+app.use(
+    rateLimiter({
+        windowMs : 15 * 60 * 1000,
+        max: 60
+    })
+)
+app.use(helment())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
+
 
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -26,14 +44,14 @@ app.use(cookieParser(process.env.JWT_SECRET))
 app.use(fileUpload())
 
 
-app.get('/',(req, res)=>{
-    res.send("Hello")
-})
 
 app.get('/api/v1',(req,res)=>{
     console.log(req.signedCookies.token);
     res.send('e-commerce api')
 })
+
+app.use(express.static('./public'))
+
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRoutes)
